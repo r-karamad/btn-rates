878fb3975de402ba267b397e86d46d00af8322cb
Reza Karamad <reza.karamad@idealo.de>
2022-09-11 23:58:04 +0200

    added test job in the workflow

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+import requests
 
-# Create your tests here.
+class SimpleTests(SimpleTestCase):
+  def test_home_page(self):
+    response = self.client.get('/')
+    self.assertEqual(response.status_code, 200)
+  
+  def test_api(self):
+    response = requests.get("https://api.coingecko.com/api/v3/exchange_rates")
+    self.assertEqual(response.status_code, 200)
